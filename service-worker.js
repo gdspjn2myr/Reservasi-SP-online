f0b458e86be72a02272f6e35c94f3758ede7a75a
gdspjn2myr <gdsp.jn2myr@gmail.com>
2026-09-14 13:38:11 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // dapat notif "Versi baru tersedia".
 // ============================================================================
 
-const APP_VERSION = '1.0.6';
+const APP_VERSION = '1.0.7';
 const CACHE_NAME = 'v-' + APP_VERSION;
 const APP_SHELL = [
   './',
